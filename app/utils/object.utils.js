00d8fb8cc521b234da313f364c43771e42f1d861
Your Name <you@example.com>
2022-01-10 14:44:27 +0200

    first commit

---
+++ app/utils/object.utils.js
@@ -0,0 +1,9 @@
+export function generateQueryConstructor(query) {
+  for (const key in query) {
+    this[key] = query[key];
+  }
+}
+
+export function generateArray(length, callbackFn) {
+  return [...new Array(length)].map(callbackFn);
+}
